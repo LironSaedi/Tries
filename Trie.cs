16ec852f84d49f026aa3cca9b12c0e2c2799ccfb
Lion Saedi <lionsaedi28@outlook.com>
2021-09-29 18:15:55 -0700

    I finished the insert and started the search node, removee, and contains functions

--- Trie.cs
+++ Trie.cs
@@ -18,34 +18,55 @@ namespace Tries
         {
             // loop to see if what the word length of any of the words are
             //check to see wheter or not the caracter exists. If it does
-            var child = root.Children;
+            //var child = root.Children;
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    var ltr = word[i];
+            //    Node triend;
+            //}
+            Node tracker = root;
+            var alltheChildren = root.Children;
+
             for (int i = 0; i < word.Length; i++)
             {
                 var ltr = word[i];
-                Node triend;
+                
+                if(!tracker.Children.ContainsKey(ltr)) // checks if it is contained in the trie
+                {
+                    tracker.Children.Add(ltr, new Node(ltr)); // adds teh tracker at the child
+                }
+
+                tracker = tracker.Children[ltr]; // move tracker
+
+                if(i == word.Length - 1)
+                {
+                    tracker.IsWord = true;
+                }
             }
 
-            if (child.)
+            
         }
-
+        
         public bool Contains(string word)
         {
-
+            var searchnode = SearchNode(word);
         }
-
+        
         private Node SearchNode(string word)
         {
-
+            var temporaryChildren = root.Children;
         }
-
+        /*
         public List<string> GetAllMatchingPrefix(string prefix)
         {
 
         }
-
+        */
         public bool Remove(string prefix)
         {
-
+            var foundnode = SearchNode(prefix);
+            if(foundnode == null)
         }
+        
     }
 }
